40ae514bb843cbab7f66b41de8a3e8764bbfb436
Jasmine Wongphatarakul <110138906+jjasminew@users.noreply.github.com>
2022-10-12 05:21:37 +0000

    Updated individual projects pages

--- script.js
+++ script.js
@@ -5,3 +5,8 @@ function openNav() {
 function closeNav() {
   document.getElementById("mySideNav").style.width = "0";
 }
+
+var quadimages = document.querySelectorAll("#quad figure");
+for(i=0; i<quadimages.length; i++) {
+  quadimages[i].addEventListener('click', function(){ this.classList.toggle("expanded"); quad.classList.toggle("full") }); 
+}
